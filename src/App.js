db4a927a8803827c3d2b943e7d31c9d185c7be40
ali <moradkhani.mohammadali@gmail.com>
2023-12-29 23:30:49 +0330

    complete Login page ui and add protected routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Routes , Route } from 'react-router-dom';
+import React, { useState } from 'react';
 import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import ProtectedRoutes from './Routes/ProtectedRoutes';
+import { Routes , Route } from 'react-router-dom';
 
 const App = () => {
+    const [isAuthenticated , setIsAuthenticated] = useState(false);
+
     return (
         <Routes>
-            <Route path='/' element={<Login />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/' 
+            element={
+                <ProtectedRoutes auth={isAuthenticated}>
+                    <Home />
+                </ProtectedRoutes>
+            }/>
         </Routes>
     )
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import styles from './Login.module.css';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineLogin } from "react-icons/hi";
 
 const Login = () => {
     return (
         <div id={styles.login}>
             <form className={styles.form}>
+                <section className={styles.header}>
+                    <div className={styles.titleContainer}>
+                        <h1 className={styles.title}>ورود</h1>
+                        <HiOutlineLogin className={styles.loginIcon}/>
+                    </div>
+                    <div className={styles.linkContainer}>
+                        <p>حساب کاربری ندارید؟</p>
+                        <NavLink className={styles.link} to={'/register'}>
+                            ثبت نام
+                        </NavLink>
+                    </div>
+                </section>
                 <section className={styles.fieldContainer}>
                     <input type='email' className={'common-field'}/>
-                    <p></p>
+                    <p className={styles.errorMessage}></p>
                 </section>
                 <section className={styles.fieldContainer}>
                     <input type='password' className={'common-field'}/>
-                    <p></p>
+                    <p className={styles.errorMessage}></p>
                 </section>
+                <button className={'form-button'} type='button'>
+                    ورود
+                </button>
             </form>
         </div>
     )
