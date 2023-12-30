32875bc697d78b704211e3280d55cc0c35250a96
ali <moradkhani.mohammadali@gmail.com>
2023-12-30 22:27:02 +0330

    add placeholder , icons into fields

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './Login.module.css';
 import { NavLink } from 'react-router-dom';
 import { HiOutlineLogin } from "react-icons/hi";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { GoLock } from "react-icons/go";
 
 const Login = () => {
     return (
         <div id={styles.login}>
-            <form className={styles.form}>
+            <form className={'common-form'}>
                 <section className={styles.header}>
                     <div className={styles.titleContainer}>
                         <h1 className={styles.title}>ورود</h1>
@@ -20,12 +22,14 @@ const Login = () => {
                     </div>
                 </section>
                 <section className={styles.fieldContainer}>
-                    <input type='email' className={'common-field'}/>
+                    <input type='email' className={'common-field'} placeholder='آدرس ایمیل'/>
                     <p className={styles.errorMessage}></p>
+                    <MdOutlineMailOutline className={styles.fieldIcon}/>
                 </section>
                 <section className={styles.fieldContainer}>
-                    <input type='password' className={'common-field'}/>
+                    <input type='password' className={'common-field'} placeholder='رمز عبور'/>
                     <p className={styles.errorMessage}></p>
+                    <GoLock className={styles.fieldIcon}/>
                 </section>
                 <button className={'form-button'} type='button'>
                     ورود
