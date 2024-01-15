39a5ae4de9eadc6f60cbdd2942af629d5b799bc6
ali <moradkhani.mohammadali@gmail.com>
2024-01-15 23:39:15 +0330

    add error message for fields

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css';
 import { NavLink } from 'react-router-dom';
 import { HiOutlineLogin } from "react-icons/hi";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { GoLock } from "react-icons/go";
+import loginSchema from '../../Validation/loginValidation';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Login = () => {
+    const [userData , setUserData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const [errors , setErrors] = useState({});
+    const [isPending , setIsPending] = useState(false);
+
+    const handleValidation = async () => {
+        try {
+            let validation = await loginSchema.validate(userData , {
+                abortEarly: false,
+            });
+            setErrors({});
+            setIsPending(true);
+        }
+        catch (err) {
+            let errs = err.inner.reduce((acc , error) => ({
+                ...acc ,
+                [error.path]: error.message,
+            }) , {})
+            setErrors(errs);
+            setIsPending(false);
+        }
+    }
+
+    const handleChangeUserData = event => {
+        setUserData(prev => ({...prev , [event.target.name]: event.target.value}))
+    }
+    
+    const handleSubmit = async event => {
+        event.preventDefault();
+        handleValidation();
+    }
+
     return (
         <div id={styles.login}>
-            <form className={'common-form'}>
+            <form className={'common-form'} onSubmit={handleSubmit}>
                 <section className={styles.header}>
                     <div className={styles.titleContainer}>
                         <h1 className={styles.title}>ورود</h1>
@@ -22,17 +59,20 @@ const Login = () => {
                     </div>
                 </section>
                 <section className={styles.fieldContainer}>
-                    <input type='email' className={'common-field'} placeholder='آدرس ایمیل'/>
-                    <p className={styles.errorMessage}></p>
+                    <input type='email' className={'common-field'} placeholder='آدرس ایمیل'
+                    onChange={handleChangeUserData} name='email'/>
+                    <p className={styles.errorMessage}>{errors?.email ? errors.email : ''}</p>
                     <MdOutlineMailOutline className={styles.fieldIcon}/>
                 </section>
                 <section className={styles.fieldContainer}>
-                    <input type='password' className={'common-field'} placeholder='رمز عبور'/>
-                    <p className={styles.errorMessage}></p>
+                    <input type='password' className={'common-field'} placeholder='رمز عبور'
+                    onChange={handleChangeUserData} name='password'/>
+                    <p className={styles.errorMessage}>{errors?.password ? errors.password : ''}</p>
                     <GoLock className={styles.fieldIcon}/>
                 </section>
-                <button className={'form-button'} type='button'>
+                <button className={'form-button'} type='submit'>
                     ورود
+                    {isPending && <CircularProgress color="inherit" size={15}/>}
                 </button>
             </form>
         </div>
--- src/Validation/loginValidation.js
+++ src/Validation/loginValidation.js
@@ -7,14 +7,14 @@ const errors = {
     },
     password: {
         required: 'لطفا رمز عبور خود را وارد کنید',
-        min: 'تعداد کارکتر ها باید کمتر از هشت میباشد',
+        min: 'رمز عبور باید حداقل هشت کارکتر باشد',
         matches: 'کارکترها باید شامل حروف و عدد باشند'
     }
 }
 
 let loginSchema = Yup.object({
     email: Yup.string().email(errors.email.email).required(errors.email.required),
-    password: Yup.string().required(errors.password.required).min(8 , errors.password.min).matches(/^([a-zA-Z0-9\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9 _.-]+)$/ , errors.password.matches),
+    password: Yup.string().min(8 , errors.password.min).required(errors.password.required),
 })
 
 export default loginSchema;
